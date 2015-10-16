38dcb466f6299f682ac1f3786f3e528bce7e4b7c
Brian Kustra <bkustrakb@gmail.com>
2015-10-16 17:44:36 -0500

    Make it so that the enemies move to a new random location every second using.

--- client/watchout.js
+++ client/watchout.js
@@ -3,6 +3,7 @@
 var height = 450;
 var width = 700;
 var r = 10;
+var MAX_ENEMIES = 30;
 
 var svg = d3.select('body')
   .append('svg')
@@ -10,11 +11,31 @@ var svg = d3.select('body')
   .attr('height', height)
   .attr('shape-rendering', 'geometricPrecision');
 
-svg.append('circle')
-  .attr('fill', 'black')
-  .attr('r', r)
-  .attr('cx', width/2)
-  .attr('cy', height/2);
+for ( var i = 0; i < MAX_ENEMIES; i++ ) {
+  svg.append('circle')
+    .attr('fill', 'black')
+    .attr('r', r)
+    .attr('cx', Math.random() * (width - 2 * r) + r)
+    .attr('cy', Math.random() * (height - 2 * r) + r);
+}
+
+var update = function() {
+  var circles = svg.selectAll('circle');
+
+  circles.each(function() {
+    var circle = d3.select(this);
+    
+    circle
+      .transition()
+      .duration(1000)
+      .attr('cx', Math.random() * (width - 2 * r) + r)
+      .attr('cy', Math.random() * (height - 2 * r) + r);
+  });
+  
+  setTimeout(update, 1000);
+};
+
+update();
 
 // make an array of objects
   // pass to the rendering function
